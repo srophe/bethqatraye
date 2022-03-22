10ce8b2ec3be1238a1fdc294cb31620f4c1bac18
Winona Salesky <wsalesky@gmail.com>
2022-03-22 16:53:47 -0400

    Suppress spinner on page load.

--- resources/js/srophe.js
+++ resources/js/srophe.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 // Main javascript functions used by place pages
 // validate contact forms
+$('.spinning').hide();
 $.validator.setDefaults({
 	submitHandler: function() {
          	//Ajax submit for contact form
